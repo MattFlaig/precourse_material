b1beca863dcab09707f567788ea3c91f2fb56fdd
MattFlaig <matt.flaig@web.de>
2013-09-28 14:47:30 +0200

    Chapter 5: favourite number

---
+++ c5_favouritenumber.rb
@@ -0,0 +1,4 @@
+puts 'Hello there, and what\'s your favourite number?'
+number = gets.chomp.to_i
+num = number + 1
+puts 'But you know what? You should pick ' + num.to_s + 'as a favourite number instead, because it is a bigger and better number then yours!' 
